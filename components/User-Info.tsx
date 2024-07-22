49625b455c4bc7a9c3a717d80d5257ea06c4cdb5
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-07-22 16:54:47 -0700

    started working on backlog and completed lists on dashboard

--- components/User-Info.tsx
+++ components/User-Info.tsx
@@ -4,33 +4,41 @@ import { useSession } from "next-auth/react";
 
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Backlog from "./Backlog";
+import Completed from "./Completed";
 
 const UserInfo = () => {
   const { data: session, status } = useSession();
   return (
     <main>
       {status === "authenticated" ? (
-        <div className="flex min-h-screen flex-col items-center justify-center p-24">
-          {session.user.avatar && (
-            <Avatar className="w-60 h-60 rounded-full">
-              <AvatarImage src={session?.user?.avatar} />
-              <AvatarFallback>IMG</AvatarFallback>
-            </Avatar>
-          )}
-          <h1 className="text-3xl font-bold">Welcome!</h1>
-          <h1 className="text-3xl font-extrabold text-red-400 ">
-            {session?.user?.email}
-          </h1>
-          <Button
-            onClick={() => {
-              signOut();
-            }}
-            type="submit"
-            className="mt-6"
-          >
-            Logout
-          </Button>
-        </div>
+        <>
+          <div className="flex flex-col items-center justify-center p-24">
+            {session.user.avatar && (
+              <Avatar className="w-60 h-60 rounded-full">
+                <AvatarImage src={session?.user?.avatar} />
+                <AvatarFallback>IMG</AvatarFallback>
+              </Avatar>
+            )}
+            <h1 className="text-3xl font-bold">Welcome!</h1>
+            <h1 className="text-3xl font-extrabold text-red-400 ">
+              {session?.user?.email}
+            </h1>
+            <Button
+              onClick={() => {
+                signOut();
+              }}
+              type="submit"
+              className="mt-6"
+            >
+              Logout
+            </Button>
+          </div>
+          <div>
+            <Backlog />
+            <Completed />
+          </div>
+        </>
       ) : (
         <div className="flex min-h-screen flex-col items-center justify-center p-24">
           <h1 className="text-3xl font-bold">Welcome!</h1>
