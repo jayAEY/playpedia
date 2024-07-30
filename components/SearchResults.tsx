4578d0c50b1b687fb76f4b36b75527c9efd8bf87
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-07-30 16:26:16 -0700

    added suspese to fixe errors

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,6 +1,6 @@
 // "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import GameCard from "./GameCard";
 import { HowLongToBeatEntry } from "howlongtobeat";
 
@@ -1047,43 +1047,46 @@ const SearchResults = () => {
 
   console.log(data);
   return (
-    <main>
-      {loading ? (
-        <h1 className="pl-10 pt-10 text-2xl font-semibold text-muted-foreground">
-          Results Loading...
-        </h1>
-      ) : (
-        searchQuery && (
+    <Suspense>
+      {" "}
+      <main>
+        {loading ? (
           <h1 className="pl-10 pt-10 text-2xl font-semibold text-muted-foreground">
-            Search results for &ldquo;{searchQuery}&ldquo;
+            Results Loading...
           </h1>
-        )
-      )}
-      <section className="grid p-10 min-w-screen grid-cols-1 md:grid-cols-2 xl:grid-cols-3 min-[1600px]:grid-cols-4  gap-4">
-        {data?.results
-          ? data?.results.map((game, index) => {
-              return (
-                game.gameplayMain != 0 && (
-                  <GameCard
-                    name={game.name}
-                    image={game.imageUrl}
-                    gameplayMain={game.gameplayMain}
-                    gameplayMainExtra={game.gameplayMainExtra}
-                    completionist={game.gameplayCompletionist}
-                    platforms={game.platforms}
-                    id={game.id}
-                    key={game.name + index}
-                  />
-                )
-              );
-            })
-          : !loading && (
-              <h1 className="text-2xl font-semibold text-nowrap text-muted-foreground">
-                No games found
-              </h1>
-            )}
-      </section>
-    </main>
+        ) : (
+          searchQuery && (
+            <h1 className="pl-10 pt-10 text-2xl font-semibold text-muted-foreground">
+              Search results for &ldquo;{searchQuery}&ldquo;
+            </h1>
+          )
+        )}
+        <section className="grid p-10 min-w-screen grid-cols-1 md:grid-cols-2 xl:grid-cols-3 min-[1600px]:grid-cols-4  gap-4">
+          {data?.results
+            ? data?.results.map((game, index) => {
+                return (
+                  game.gameplayMain != 0 && (
+                    <GameCard
+                      name={game.name}
+                      image={game.imageUrl}
+                      gameplayMain={game.gameplayMain}
+                      gameplayMainExtra={game.gameplayMainExtra}
+                      completionist={game.gameplayCompletionist}
+                      platforms={game.platforms}
+                      id={game.id}
+                      key={game.name + index}
+                    />
+                  )
+                );
+              })
+            : !loading && (
+                <h1 className="text-2xl font-semibold text-nowrap text-muted-foreground">
+                  No games found
+                </h1>
+              )}
+        </section>
+      </main>
+    </Suspense>
   );
 };
 
