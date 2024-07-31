23359cec346feeb325094b8889b0148324d3696a
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-07-31 09:54:05 -0700

    fixed deployment error

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="system"
           >
-            <Suspense>
-              <Navbar />
-            </Suspense>
+            {/* <Suspense> */}
+            <Navbar />
+            {/* </Suspense> */}
             {children}
             <Toaster />
           </ThemeProvider>
