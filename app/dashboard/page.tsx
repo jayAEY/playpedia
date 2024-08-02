e86317255131a81b2fd70c076a3ebed51126585e
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-08-01 17:34:57 -0700

    fixed deploy error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,6 @@ import UserInfo from "@/components/User-Info";
 import authOptions from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { Payment, columns } from "../../components/work/columns";
-import { DataTable } from "./data-table";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
