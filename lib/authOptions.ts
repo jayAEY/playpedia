c3ca3824c2228c666cdf87056c934d0325f93a5c
jayAEY <106685702+jayAEY@users.noreply.github.com>
2024-08-30 13:59:01 -0700

    continued work on google login

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -27,6 +27,18 @@ const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
+
+      // profile(profile) {
+      // return {};
+      // },
+      // authorization: {
+      //   params: {
+      //     prompt: "consent",
+      //     access_type: "offline",
+      //     response_type: "code",
+      //   },
+      // },
     }),
   ],
   callbacks: {
@@ -56,6 +68,69 @@ const authOptions: NextAuthOptions = {
         completed: [];
       };
       return session;
+    },
+    async signIn({ user }) {
+      console.log("inside callback");
+      await connectToDb();
+      const u = await UsersModel.findOne({ user });
+      const { email, name, image } = user;
+      const created = new Date();
+
+      if (!user) {
+        const newUser = new UsersModel({
+          email,
+          username: name,
+          created,
+          avatar: image,
+        });
+        await newUser.save();
+      }
+
+      return true;
+
+      // export async function POST(req: NextRequest) {
+      //   const { email, username, password, avatar, created } = await req.json();
+
+      //   console.log(email, username, created);
+      //   try {
+      //     connectToDb();
+      //     const user = await UsersModel.findOne({ email });
+      //     if (user) {
+      //       return NextResponse.json(
+      //         { message: "User already exists" },
+      //         { status: 500 }
+      //       );
+      //     }
+      //     const hashedPassword = await bcrypt.hash(password, 10);
+      //     const newUser = new UsersModel({
+      //       email,
+      //       username,
+      //       password: hashedPassword,
+      //       created,
+      //       avatar,
+      //     });
+      //     await newUser.save();
+      //     return NextResponse.json(
+      //       { message: `${email} is now registered!` },
+      //       { status: 201 }
+      //     );
+      //   } catch (err) {
+      //     return NextResponse.json(
+      //       { message: "An error has occurred" },
+      //       { status: 500 }
+      //     );
+      //   }
+      // }
+      // const { email, password } = credentials as {
+      //   email: string;
+      //   password: string;
+      // };
+      // await connectToDb();
+      // const user = await UsersModel.findOne({ email });
+      // if (!user) throw new Error("User does not exist");
+      // const passwordMatch = await bcrypt.compare(password, user.password);
+      // if (!passwordMatch) throw new Error("Wrong Password");
+      // return user;
     },
   },
   session: { strategy: "jwt" },
--- models/Users.ts
+++ models/Users.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, models } from "mongoose";
 const usersSchema = new Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   created: { type: String, required: true },
   avatar: String,
   backlog: Array,
